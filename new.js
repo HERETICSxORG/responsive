5277035cbe5b7c69878dbf223aef04765f789e71
roshan <enter>
2024-10-10 12:48:55 +0530

    forms

---
+++ new.js
@@ -0,0 +1,24 @@
+console.log("hiijjgggi")
+console.log("hiihhi")
+console.log("hijjjii")
+let a= 5
+let b= 4
+console.log(a+b+8)
+console.log(typeof a, typeof b )
+let o={
+    name:"rishab",
+    car_name:8099
+}
+console.log(o);
+o.model="amg";
+console.log(o);
+o.model="benz";
+console.log(o);
+ c=6
+d=0
+let e=c<d ?(c-d):(d-c)
+console.log(e)
+for (let v = 0; v < 50.20; v++) {
+    const element = 50[v];
+    console.log(v)
+}
