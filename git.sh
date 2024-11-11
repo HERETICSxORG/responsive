99630e56254d0bf257fcb062889ef3b1748f35a5
roshan <enter>
2024-11-12 02:51:10 +0530

    basic comms git and github

---
+++ git.sh
@@ -0,0 +1,31 @@
+# write a basic script that will clone a git repository
+git clone "url"
+# code to push to a git repository
+git add .
+# git commit -m "message"
+git commit -m "message"
+# git push origin master(github)
+git push origin master
+# or git push origin main(github)
+git push origin main
+# to delete a git repository
+rm index.html
+# to revert changes
+git checkout -- index.html
+# to pull from github
+git pull origin master
+# or main
+git pull origin main
+
+# chornology of git
+git cd "folder"
+git echo "Hello World" > index.html
+git add .
+git commit -m "message"
+git push origin master
+# or main
+git push origin main
+# to delete a git repository
+rm index.html
+# to revert changes
+git checkout -- index.html
