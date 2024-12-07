1ab7bd0338f3481ff6b2bd958fbb2f55f739ef12
rsp2308 <roshan.parmar2024@lpu.in>
2024-12-07 13:31:57 +0530

    restAPIs

--- git.sh
+++ git.sh
@@ -21,6 +21,7 @@ git pull origin main
 git cd "folder"
 git echo "Hello World" > index.html
 git add .
+git init
 git commit -m "message"
 git push origin master
 # or main
@@ -40,4 +41,25 @@ git remote add origin "url"
 git remote -v   # to check the status of the git repository
 git push origin main
 git push -u origin main
+
+touch .gitignore
+echo "node_modules" > .gitignore
+echo "dist" >> .gitignore
+echo ".env" >> .gitignore
+echo ".DS_Store" >> .gitignore
+ 
+# to create a branch
+git branch "branch name"
+# to switch to a branch
+git checkout "branch name"
+# to create a branch and switch to it
+git checkout -b "branch name"
+# to merge a branch
+git merge "branch name"
+# to delete a branch
+git branch -d "branch name"
+# to list all branches
+git branch
+# to list all branches including remote branches
+git branch -a
 
