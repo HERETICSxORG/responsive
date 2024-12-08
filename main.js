ca74ef055d625c93e26c660d43f5f1b606a7e779
rsp2308 <roshan.parmar2024@lpu.in>
2024-12-08 22:02:12 +0530

    routes

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log("hello")
+// create a route 
+app.get('/', (req, res) => {
+    res.send("hello")
+}   
+)   
+// create a port
+app.listen(3000, () => {
+    console.log("server is running")
+})
