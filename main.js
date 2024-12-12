37b7679f4da21e23d50d07021f219c9e8bc11016
rsp2308 <roshan.parmar2024@lpu.in>
2024-12-12 23:57:22 +0530

    cors

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+import cors from 'cors';
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+ 
+// create express app
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
 console.log("hello")
 // create a route 
 app.get('/', (req, res) => {
